f8c6496a668b7080aad68644cda215d861a824ae
PanGan21 <ganelis.pan@gmail.com>
2023-12-25 16:06:20 +0200

    add double map

--- pallets/double-map/src/lib.rs
+++ pallets/double-map/src/lib.rs
@@ -48,6 +48,9 @@ pub mod pallet {
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		NewMember(T::AccountId),
+		MemberJoinGroup(T::AccountId, GroupIndex, u32),
+		RemoveMember(T::AccountId),
+		RemoveGroup(GroupIndex),
 	}
 
 	#[pallet::call]
@@ -67,8 +70,35 @@ pub mod pallet {
 		#[pallet::weight(10_000)]
 		pub fn join_group(origin: OriginFor<T>, index: GroupIndex, score: u32) -> DispatchResult {
 			let member = ensure_signed(origin)?;
-			ensure!(!Self::is_member(&member), "not a member, can't join group");
+			ensure!(Self::is_member(&member), "not a member, can't join group");
+			<MemberScore<T>>::insert(&index, &member, score);
+			<GroupMembership<T>>::insert(&member, &index);
 
+			Self::deposit_event(Event::MemberJoinGroup(member, index, score));
+			Ok(())
+		}
+
+		#[pallet::call_index(2)]
+		#[pallet::weight(10_000)]
+		pub fn remove_member(origin: OriginFor<T>) -> DispatchResult {
+			let member_to_remove = ensure_signed(origin)?;
+			ensure!(Self::is_member(&member_to_remove), "not a member, can't remove");
+			let group_id = <GroupMembership<T>>::take(&member_to_remove);
+			<MemberScore<T>>::remove(group_id, &member_to_remove);
+
+			Self::deposit_event(Event::RemoveMember(member_to_remove));
+			Ok(())
+		}
+
+		#[pallet::call_index(3)]
+		#[pallet::weight(10_000)]
+		pub fn remove_group_score(origin: OriginFor<T>, group: GroupIndex) -> DispatchResult {
+			let member = ensure_signed(origin)?;
+			let group_id = <GroupMembership<T>>::get(&member);
+			ensure!(group_id == group, "member isn't in the group, can't remove it");
+			let _ = <MemberScore<T>>::clear_prefix(&group_id, u32::MAX, None);
+
+			Self::deposit_event(Event::RemoveGroup(group_id));
 			Ok(())
 		}
 	}
--- pallets/double-map/src/tests.rs
+++ pallets/double-map/src/tests.rs
@@ -1,4 +1,4 @@
-use crate::{mock::*, Event};
+use crate::{mock::*, Event, GroupMembership, MemberScore};
 use frame_support::{assert_err, assert_ok};
 
 #[test]
@@ -11,5 +11,64 @@ fn join_all_members_works() {
 		);
 		System::assert_last_event(Event::<Test>::NewMember(1).into());
 		assert_eq!(DoubleMap::all_members(), vec![1]);
+	})
+}
+
+#[test]
+fn join_group_works() {
+	new_test_ext().execute_with(|| {
+		assert_err!(
+			DoubleMap::join_group(RuntimeOrigin::signed(1), 3, 5),
+			"not a member, can't join group"
+		);
+
+		assert_ok!(DoubleMap::join_all_members(RuntimeOrigin::signed(1)));
+		assert_ok!(DoubleMap::join_group(RuntimeOrigin::signed(1), 3, 5));
+		System::assert_last_event(Event::<Test>::MemberJoinGroup(1, 3, 5).into());
+		assert_eq!(DoubleMap::group_membership(1), 3);
+		assert_eq!(DoubleMap::member_score(3, 1), 5);
+	})
+}
+
+#[test]
+fn remove_member_works() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(DoubleMap::join_all_members(RuntimeOrigin::signed(1)));
+		assert_ok!(DoubleMap::join_group(RuntimeOrigin::signed(1), 3, 5));
+		assert_ok!(DoubleMap::remove_member(RuntimeOrigin::signed(1)));
+		System::assert_last_event(Event::<Test>::RemoveMember(1).into());
+		assert!(!<GroupMembership<Test>>::contains_key(1));
+		assert!(!<MemberScore<Test>>::contains_key(3, 1));
+	})
+}
+
+#[test]
+fn remove_group_score_works() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(DoubleMap::join_all_members(RuntimeOrigin::signed(1)));
+		assert_ok!(DoubleMap::join_all_members(RuntimeOrigin::signed(2)));
+		assert_ok!(DoubleMap::join_all_members(RuntimeOrigin::signed(3)));
+		assert_ok!(DoubleMap::join_group(RuntimeOrigin::signed(1), 3, 5));
+		assert_ok!(DoubleMap::join_group(RuntimeOrigin::signed(2), 3, 5));
+		assert_ok!(DoubleMap::join_group(RuntimeOrigin::signed(3), 3, 5));
+
+		assert_err!(
+			DoubleMap::remove_group_score(RuntimeOrigin::signed(4), 3),
+			"member isn't in the group, can't remove it"
+		);
+
+		assert_err!(
+			DoubleMap::remove_group_score(RuntimeOrigin::signed(1), 2),
+			"member isn't in the group, can't remove it"
+		);
+
+		assert_ok!(DoubleMap::remove_group_score(RuntimeOrigin::signed(1), 3));
+
+		System::assert_last_event(Event::<Test>::RemoveGroup(3).into());
+
+		// check: user 1, 2, 3 should no longer in the group
+		assert!(!<MemberScore<Test>>::contains_key(3, 1));
+		assert!(!<MemberScore<Test>>::contains_key(3, 2));
+		assert!(!<MemberScore<Test>>::contains_key(3, 3));
 	})
 }
