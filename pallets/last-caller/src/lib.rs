27cfcb3590cfbafde1e44d467c48a5564de3992a
PanGan21 <ganelis.pan@gmail.com>
2023-12-28 21:23:18 +0200

    make last-caller pallet instatiatable

--- pallets/last-caller/src/lib.rs
+++ pallets/last-caller/src/lib.rs
@@ -8,24 +8,25 @@ pub mod pallet {
 	use frame_system::pallet_prelude::*;
 
 	#[pallet::pallet]
-	pub struct Pallet<T>(_);
+	pub struct Pallet<T, I = ()>(_);
 
 	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+	pub trait Config<I: 'static = ()>: frame_system::Config {
+		type RuntimeEvent: From<Event<Self, I>>
+			+ IsType<<Self as frame_system::Config>::RuntimeEvent>;
 	}
 
 	#[pallet::storage]
-	pub type Caller<T: Config> = StorageValue<_, T::AccountId, OptionQuery>;
+	pub type Caller<T: Config<I>, I: 'static = ()> = StorageValue<_, T::AccountId, OptionQuery>;
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {
+	pub enum Event<T: Config<I>, I: 'static = ()> {
 		Called(T::AccountId),
 	}
 
 	#[pallet::call]
-	impl<T: Config> Pallet<T> {
+	impl<T: Config<I>, I: 'static> Pallet<T, I> {
 		#[pallet::call_index(0)]
 		#[pallet::weight(10_000)]
 		pub fn call_pal(origin: OriginFor<T>) -> DispatchResult {
@@ -33,7 +34,7 @@ pub mod pallet {
 
 			// When writing to storage, we supply, not only a configuration T, but also an
 			// instance, I.
-			<Caller<T>>::put(&caller);
+			<Caller<T, I>>::put(&caller);
 			Self::deposit_event(Event::Called(caller));
 			Ok(().into())
 		}
