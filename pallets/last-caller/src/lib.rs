630bf20155f355d26aad6d69e499550998159a03
PanGan21 <ganelis.pan@gmail.com>
2023-12-28 20:58:20 +0200

    add last caller pallet

---
+++ pallets/last-caller/src/lib.rs
@@ -0,0 +1,41 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[frame_support::pallet(dev_mode)]
+pub mod pallet {
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+
+	#[pallet::pallet]
+	pub struct Pallet<T>(_);
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+	}
+
+	#[pallet::storage]
+	pub type Caller<T: Config> = StorageValue<_, T::AccountId, OptionQuery>;
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		Called(T::AccountId),
+	}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::call_index(0)]
+		#[pallet::weight(10_000)]
+		pub fn call_pal(origin: OriginFor<T>) -> DispatchResult {
+			let caller = ensure_signed(origin)?;
+
+			// When writing to storage, we supply, not only a configuration T, but also an
+			// instance, I.
+			<Caller<T>>::put(&caller);
+			Self::deposit_event(Event::Called(caller));
+			Ok(().into())
+		}
+	}
+}
