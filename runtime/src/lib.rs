74c3f3f20206f601f08bb982c850c757da5fcece
PanGan21 <ganelis.pan@gmail.com>
2023-12-29 12:03:46 +0200

    add last caller instances in runtime

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -49,6 +49,8 @@ pub use sp_runtime::{Perbill, Permill};
 /// Import the template pallet.
 pub use pallet_template;
 
+pub use last_caller;
+
 /// An index to a block.
 pub type BlockNumber = u32;
 
@@ -274,6 +276,14 @@ impl pallet_template::Config for Runtime {
 	type WeightInfo = pallet_template::weights::SubstrateWeight<Runtime>;
 }
 
+impl last_caller::Config<last_caller::Instance1> for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+}
+
+impl last_caller::Config<last_caller::Instance2> for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub struct Runtime {
@@ -286,6 +296,8 @@ construct_runtime!(
 		Sudo: pallet_sudo,
 		// Include the custom logic from the pallet-template in the runtime.
 		TemplateModule: pallet_template,
+		LastCallerInstance1: last_caller::<Instance1>,
+		LastCallerInstance2: last_caller::<Instance2>,
 	}
 );
 
